1f12c660c37cfa01e1fc4a261d523ef38721e5bf
ychagri <ychagri@student.1337.ma>
2024-04-28 00:30:36 +0100

    pipekhs

--- main.c
+++ main.c
@@ -6,16 +6,26 @@
 /*   By: ychagri <ychagri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 20:51:48 by ychagri           #+#    #+#             */
-/*   Updated: 2024/04/28 00:10:36 by ychagri          ###   ########.fr       */
+/*   Updated: 2024/04/28 00:29:28 by ychagri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mandatory/Inc/pipex.h"
 
-int	main(int ac, char **av)
+int	main()
 {
-   if (ac != 5)
-        return(ft_putstr_fd("Error : invalid number of arguments.\n", 2), 1);
-    check_files(av);
+//    if (ac != 5)
+//         return(ft_putstrint ac, char **av_fd("Error : invalid number of arguments.\n", 2), 1);
+//     check_files(av);
+
+char    **dirs;
+dirs = ft_split("/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin", ':');
+
+int i = 0;
+while (dirs[i])
+{
+    printf("%s\n", dirs[i]);
+    i++;
+}
     return 0;
 }
