d9d6480c358154b2fbd29eac2e979f9fe4263972
youssra chagri <youssrachagri@gmail.com>
2024-06-18 19:15:54 +0100

    pipekhs

--- mandatory/tools/multipipe.c
+++ mandatory/tools/multipipe.c
@@ -6,7 +6,7 @@
 /*   By: youssra <youssra@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 18:27:05 by ychagri           #+#    #+#             */
-/*   Updated: 2024/06/17 23:37:01 by youssra          ###   ########.fr       */
+/*   Updated: 2024/06/18 19:13:53 by youssra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,55 +24,51 @@ void	forking2(int fd[2])
 	close(fd[0]);
 }
 
-void	forking(char *cmd, char **argv, char **env, int flag, int ac)
+void	forking(int cmd, char **argv, char **env, int infile)
 {
 	int		fd[2];
 	pid_t	pid;
-    int     infile;
 
 	if (pipe(fd) == -1)
 		return (ft_putstr_fd("pipe() has failed!!\n", 2), exit(1));
 	pid = fork();
 	if (pid == -1)
-		return (close(fd[1]), close(fd[0]), ft_putstr_fd("fork() has failed!!\n", 2), exit(1));
+		return (close(fd[1]), close(fd[0]),
+			ft_putstr_fd("fork() has failed!!\n", 2), exit(1));
 	if (pid == 0)
 	{
 		close(fd[0]);
-        if (flag == INFILE)
-        {
-            infile = open(argv[1], O_RDONLY);
+		if (cmd == 2)
+		{
+			infile = open(argv[1], O_RDONLY);
 			if (infile == -1)
-			{
-				close(fd[1]);
-				exit(1);
-			}
-            dup2(infile, STDIN_FILENO);
+				return (close(fd[1]), exit(1));
+			dup2(infile, STDIN_FILENO);
 			close(infile);
-        }
+		}
 		dup2(fd[1], STDOUT_FILENO);
 		close(fd[1]);
-		exec_cmds(cmd, env);
+		exec_cmds(argv[cmd], env);
 	}
-	else
-		forking2(fd);
+	forking2(fd);
 }
 
 void	multipipe(char **argv, int ac, char **env)
 {
 	int	cmd;
+	int	infile;
 
-	
 	cmd = 2;
 	while (argv[cmd] && cmd < ac - 2)
-    {
-        if (cmd == 2)
+	{
+		if (cmd == 2)
 		{
 			check_files(argv, ac, INFILE);
-            forking(argv[cmd], argv, env, INFILE, ac);
+			forking(cmd, argv, env, infile);
 		}
-        else
-            forking(argv[cmd], argv, env, 4, ac);
+		else
+			forking(cmd, argv, env, 0);
 		cmd++;
-    }
+	}
 	cmd_outfile(argv, env, ac);
 }
